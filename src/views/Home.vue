04cf52b2a2e29947dbb28f1c0c1299cfab562219
ben_astengo <astengo93@gmail.com>
2022-02-10 22:28:16 -0600

    Adds index, create and show actions to places front end

---
+++ src/views/Home.vue
@@ -0,0 +1,68 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      places: [],
+      newPlacesParams: {},
+      currentPlace: {},
+    };
+  },
+  created: function () {
+    this.placesIndex();
+  },
+  methods: {
+    placesIndex: function () {
+      axios.get("http://localhost:3000/places").then((response) => {
+        console.log("Sanity Check", response.data);
+        this.places = response.data;
+      });
+    },
+    placesCreate: function () {
+      axios
+        .post("http://localhost:3000/places", this.newPlacesParams)
+        .then((response) => {
+          console.log("Places Create", response);
+          this.places.push(response.data);
+          this.newPlacesParams = {};
+        })
+        .catch((error) => {
+          console.log("Places create error", error.response);
+        });
+    },
+    placesShow: function (place) {
+      this.currentPlace = place;
+      document.querySelector("#place-details").showModal();
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>Add some places if you wanna</h1>
+    <div>
+      Name of Place:
+      <input type="text" v-model="newPlacesParams.name" />
+      Address:
+      <input type="text" v-model="newPlacesParams.address" />
+      <button v-on:click="placesCreate()">Create Place</button>
+    </div>
+    <h1>{{ "Here are some of Bens favorite places" }}</h1>
+    <div v-for="place in places" v-bind:key="place.id">
+      <h2>{{ place.name }}</h2>
+      <button v-on:click="placesShow(place)">Show Info</button>
+    </div>
+    <dialog id="place-details">
+      <form method="dialog">
+        <h1>Place info</h1>
+        <p>Name: {{ currentPlace.name }}</p>
+        <p>Address: {{ currentPlace.address }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
+  </div>
+</template>
+
+<style></style>
